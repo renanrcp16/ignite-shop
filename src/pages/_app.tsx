60d1d467a83d27da3d89e4d1b495ca7a86b964fe
Renan.Correa <renan.rcp16@gmail.com>
2022-12-02 08:11:03 -0300

    customizando tela de produtos

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app'
+import '../styles/globals.css'
+import logoImg from '../assets/logo.svg'
+import Image from 'next/image'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	return (
+		<div className='flex flex-col items-center min-h-screen justify-center'>
+			<header className='py-8 w-full max-w-[1180px] mx-auto'>
+				<Image src={logoImg} alt="" />
+			</header>
+			<Component {...pageProps} />
+		</div>
+	)
 }
+
+
+// max-w-[calc(100vw-((100vw-1180)/2))] 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,48 @@
+import Image from 'next/image'
+import camisa1 from '../assets/camisetas/camisa-1.png'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <main className='flex gap-12 max-w-[calc(100vw-((100vw-1180px)/2))] w-full ml-auto min-h-[656px]'>
+		<a className='group bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg p-1 cursor-pointer relative flex items-center overflow-hidden'>
+			<Image src={camisa1} width={520} height={480} alt='' className='object-cover'/>
+			<footer 
+				className='
+					absolute bottom-1 left-1 right-1 rounded-md flex justify-between items-center 
+					bg-[rgba(0,0,0,0.6)] p-8 translate-y-[110%] opacity-0 transition group-hover:translate-y-0
+					group-hover:opacity-100
+				'
+			>
+				<strong className='text-xl'>Camiseta X</strong>
+				<span className='font-bold text-2xl text-green-400'>R$ 79,90</span>
+			</footer>
+		</a>
+		<a className='group bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg p-1 cursor-pointer relative flex items-center overflow-hidden'>
+			<Image src={camisa1} width={520} height={480} alt='' className='object-cover'/>
+			<footer 
+				className='
+					absolute bottom-1 left-1 right-1 rounded-md flex justify-between items-center 
+					bg-[rgba(0,0,0,0.6)] p-8 translate-y-[110%] opacity-0 transition group-hover:translate-y-0
+					group-hover:opacity-100
+				'
+			>
+				<strong className='text-xl'>Camiseta X</strong>
+				<span className='font-bold text-2xl text-green-400'>R$ 79,90</span>
+			</footer>
+		</a>
+		<a className='group bg-gradient-to-r from-cyan-500 to-blue-500 rounded-lg p-1 cursor-pointer relative flex items-center overflow-hidden'>
+			<Image src={camisa1} width={520} height={480} alt='' className='object-cover'/>
+			<footer 
+				className='
+					absolute bottom-1 left-1 right-1 rounded-md flex justify-between items-center 
+					bg-[rgba(0,0,0,0.6)] p-8 translate-y-[110%] opacity-0 transition group-hover:translate-y-0
+					group-hover:opacity-100
+				'
+			>
+				<strong className='text-xl'>Camiseta X</strong>
+				<span className='font-bold text-2xl text-green-400'>R$ 79,90</span>
+			</footer>
+		</a>
+	</main>
   )
 }
